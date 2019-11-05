eac54fcfe43b83573b74464e408fab4a52ec79a9
unknown <vbothe91@gmail.com>
2019-11-06 00:31:59 +0530

    Updates

---
+++ precache-manifest.a2be0592cd34cab9e1f1603c9555ef0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5d8993891dafa277f977ef51bf8286",
+    "url": "/react-simple-calculator/index.html"
+  },
+  {
+    "revision": "f773bdc5b3fde843c0d9",
+    "url": "/react-simple-calculator/static/css/main.666b12fb.chunk.css"
+  },
+  {
+    "revision": "188db90ef085a15e9f0a",
+    "url": "/react-simple-calculator/static/js/2.92b12063.chunk.js"
+  },
+  {
+    "revision": "f773bdc5b3fde843c0d9",
+    "url": "/react-simple-calculator/static/js/main.ab805909.chunk.js"
+  },
+  {
+    "revision": "7df2238f3b7bee73e7da",
+    "url": "/react-simple-calculator/static/js/runtime-main.c28dd9b5.js"
+  }
+]);
